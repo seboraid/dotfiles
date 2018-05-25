0d3472e7762d5c49ab79a376fbda498d6a872a27
Sebastian Perez <seboraid@gmail.com>
2018-05-24 20:19:37 -0500

    newsssss

--- setup.sh
+++ setup.sh
@@ -301,13 +301,13 @@ install_zsh () {
 
 # Package managers & packages
 
-. "$DOTFILES_DIR/install/brew.sh"
+#. "$DOTFILES_DIR/install/brew.sh"
 #. "$DOTFILES_DIR/install/npm.sh"
-. "$DOTFILES_DIR/install/nodenv.sh"
+#. "$DOTFILES_DIR/install/nodenv.sh"
 
-if [ "$(uname)" == "Darwin" ]; then
-    . "$DOTFILES_DIR/install/brew-cask.sh"
-fi
+# if [ "$(uname)" == "Darwin" ]; then
+#     . "$DOTFILES_DIR/install/brew-cask.sh"
+# fi
 
 main
 install_zsh
@@ -328,7 +328,7 @@ install_zsh
 ###############################################################################
 
 # Install Zsh settings
-#ln -s ~/dotfiles/zsh/themes/nick.zsh-theme $HOME/.oh-my-zsh/themes
+ln -s ~/dotfiles/zsh/themes/nick.zsh-theme $HOME/.oh-my-zsh/themes
 
 
 ###############################################################################
