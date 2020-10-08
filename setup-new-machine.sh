137fb9be1ce19a3ecd7969a2d5d3b3a1fda3fa9a
seboraid <seboraid@gmail.com>
2020-10-07 19:28:21 -0500

    Prevent xcode comman line tools to run in linux

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -35,37 +35,39 @@ cp ~/.z ~/migration # z history file.
 # XCode Command Line Tools                                                    #
 ###############################################################################
 
-if ! xcode-select --print-path &> /dev/null; then
+if [ "$(uname)" == "Darwin" ]; then
+  if ! xcode-select --print-path &> /dev/null; then
 
-  # Prompt user to install the XCode Command Line Tools
-  xcode-select --install &> /dev/null
+    # Prompt user to install the XCode Command Line Tools
+    xcode-select --install &> /dev/null
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  # Wait until the XCode Command Line Tools are installed
-  until xcode-select --print-path &> /dev/null; do
-    sleep 5
-  done
+    # Wait until the XCode Command Line Tools are installed
+    until xcode-select --print-path &> /dev/null; do
+      sleep 5
+    done
 
-  print_result $? 'Install XCode Command Line Tools'
+    print_result $? 'Install XCode Command Line Tools'
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  # Point the `xcode-select` developer directory to
-  # the appropriate directory from within `Xcode.app`
-  # https://github.com/alrra/dotfiles/issues/13
+    # Point the `xcode-select` developer directory to
+    # the appropriate directory from within `Xcode.app`
+    # https://github.com/alrra/dotfiles/issues/13
 
-  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-  print_result $? 'Make "xcode-select" developer directory point to Xcode'
+    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+    print_result $? 'Make "xcode-select" developer directory point to Xcode'
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  # Prompt user to agree to the terms of the Xcode license
-  # https://github.com/alrra/dotfiles/issues/10
+    # Prompt user to agree to the terms of the Xcode license
+    # https://github.com/alrra/dotfiles/issues/10
 
-  sudo xcodebuild -license
-  print_result $? 'Agree with the XCode Command Line Tools licence'
+    sudo xcodebuild -license
+    print_result $? 'Agree with the XCode Command Line Tools licence'
 
+  fi
 fi
 
 ###############################################################################
